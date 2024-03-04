184303c9f2bf0a5217f35c9bd96a96bdf1486058
evoronova0202 <131252557+evoronova0202@users.noreply.github.com>
2024-03-04 23:17:05 +0300

    feat: add binary in task

--- src/queue.js
+++ src/queue.js
@@ -13,21 +13,99 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+class BinarySearchTree {
+  constructor() {
+    this.rootTree = null;
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  root() {
+    return this.rootTree;
   }
+  add( data ) {
+    this.rootTree = insert(this.rootTree, data);
+    function insert(node, data) {
+      if (node === null) {
+        return new Node(data);
+      }
+      if (data < node.data) {
+        node.left = insert(node.left, data);
+      } else if (data > node.data) {
+        node.right = insert(node.right, data);
+      }
+      return node;
+    }
+  }
+  has( data ) {
+    function search(node, data) {
+      if (node === null) {
+        return false;
+      }
+      if (data === node.data) {
+        return true;
+      } else if (data < node.data) {
+        return search(node.left, data);
+      } else {
+        return search(node.right, data);
+      }
+    }
+    return search(this.rootTree, data);
+  }
+  find( data ) {
+    if(!this.rootTree) return false;
+    let currentRoot = this.rootTree;
+    let result = false;
+    while(currentRoot && !result) {
+      if(data < currentRoot.data) {
+        currentRoot = currentRoot.left;
+      } else if(data > currentRoot.data) {
+        currentRoot = currentRoot.right;
+      } else {
+        result = currentRoot;
+      }
+    }
+    return (!result)? null : result;
+  }
+  remove( data ) {
+    this.rootTree = removeNode(this.rootTree, data);
+    function removeNode(root, data) {
+      if (data > root.data) {
+        root.right = removeNode(root.right, data);
+        return root;
+      } else if (data < root.data) {
+        root.left = removeNode(root.left, data);
+        return root;
+      } else {
+        if (!root.left && !root.right) return null;
 
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+        if (!root.left) {
+          return root.right;
+        }
+        if (!root.right) {
+          return root.left;
+        }
+
+        let minRight = root.right;
+        while (minRight.left) {
+          minRight = minRight.left;
+        }
+        root.data = minRight.data;
+        root.right = removeNode(root.right, minRight.data);
+        return root;
+      }
+    }
+  }
+  min() {
+    let node = this.rootTree;
+    while (node.left) {
+      node = node.left;
+    }
+    return node.data;
+  }
+  max() {
+    let node = this.rootTree;
+    while (node.right) {
+      node = node.right;
+    }
+    return node.data;
   }
 }
 
