6154f82810bbf9684c04461bf99e8f7b60d78a96
evoronova0202 <131252557+evoronova0202@users.noreply.github.com>
2024-03-04 23:43:26 +0300

    feat: add solutions in task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,42 +7,102 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+	constructor() {
+	  this.head = null;
+	}
+	root() {
+	  return this.head;
+	}
+	add(data) {
+	  this.head = insert(this.head, data);
+	  function insert(node, data) {
+		if (node === null) {
+		  return new Node(data);
+		}
+		if (data < node.data) {
+		  node.left = insert(node.left, data);
+		} else if (data > node.data) {
+		  node.right = insert(node.right, data);
+		}
+		return node;
+	  }
+	}
+	has(data) {
+	  function search(node, data) {
+		if (node === null) {
+		  return false;
+		}
+		if (data === node.data) {
+		  return true;
+		} else if (data < node.data) {
+		  return search(node.left, data);
+		} else {
+		  return search(node.right, data);
+		}
+	  }
+	  return search(this.head, data);
+	}
+	find(data) {
+	  if(!this.head) return false;
+	  let currentRoot = this.head;
+	  let result = false;
+	  while(currentRoot && !result) {
+		if(data < currentRoot.data) {
+		  currentRoot = currentRoot.left;
+		} else if(data > currentRoot.data) {
+		  currentRoot = currentRoot.right;
+		} else {
+		  result = currentRoot;
+		}
+	  }
+	  if(!result){
+		return null;
+	  } else {
+		return result;
+	  }
+	}
+	remove(data) {
+	  this.head = removeNode(this.head, data);
+	  function removeNode(root, data) {
+		if (data > root.data) {
+		  root.right = removeNode(root.right, data);
+		  return root;
+		} else if (data < root.data) {
+		  root.left = removeNode(root.left, data);
+		  return root;
+		} else {
+		  if (!root.left && !root.right) return null;
+		  if (!root.left) {
+			return root.right;
+		  }
+		  if (!root.right) {
+			return root.left;
+		  }
+		  let min = root.right;
+		  while (min.left) {
+			min = min.left;
+		  }
+		  root.data = min.data;
+		  root.right = removeNode(root.right, min.data);
+		  return root;
+		}
+	  }
+	}
+	min() {
+	  let node = this.head;
+	  while (node.left) {
+		node = node.left;
+	  }
+	  return node.data;
+	}
+	max() {
+	  let node = this.head;
+	  while (node.right) {
+		node = node.right;
+	  }
+	  return node.data;
+	}
   }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-}
 
 module.exports = {
   BinarySearchTree
--- src/queue.js
+++ src/queue.js
@@ -13,101 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class BinarySearchTree {
-  constructor() {
-    this.rootTree = null;
+class Queue {
+	constructor () {
+	  this.length = 0;
+	  this.rootTree = null;
+	  this.tail = null;
+	  this.node = null;
+	}
+	getUnderlyingList() {
+	  return this.rootTree;
+	}
+	enqueue(value) {
+	  this.node = new ListNode(value);
+	  if (this.rootTree) {
+		this.tail.next = this.node;
+		this.tail = this.node;
+	  } else {
+		this.rootTree = this.node;
+		this.tail = this.node;
+	  }
+	  ++this.length;
+	}
+	dequeue() {
+	  const queue = this.rootTree;
+	  this.rootTree = this.rootTree.next;
+	  --this.length;
+	  return queue.value;
+	}
   }
-  root() {
-    return this.rootTree;
-  }
-  add( data ) {
-    this.rootTree = insert(this.rootTree, data);
-    function insert(node, data) {
-      if (node === null) {
-        return new Node(data);
-      }
-      if (data < node.data) {
-        node.left = insert(node.left, data);
-      } else if (data > node.data) {
-        node.right = insert(node.right, data);
-      }
-      return node;
-    }
-  }
-  has( data ) {
-    function search(node, data) {
-      if (node === null) {
-        return false;
-      }
-      if (data === node.data) {
-        return true;
-      } else if (data < node.data) {
-        return search(node.left, data);
-      } else {
-        return search(node.right, data);
-      }
-    }
-    return search(this.rootTree, data);
-  }
-  find( data ) {
-    if(!this.rootTree) return false;
-    let currentRoot = this.rootTree;
-    let result = false;
-    while(currentRoot && !result) {
-      if(data < currentRoot.data) {
-        currentRoot = currentRoot.left;
-      } else if(data > currentRoot.data) {
-        currentRoot = currentRoot.right;
-      } else {
-        result = currentRoot;
-      }
-    }
-    return (!result)? null : result;
-  }
-  remove( data ) {
-    this.rootTree = removeNode(this.rootTree, data);
-    function removeNode(root, data) {
-      if (data > root.data) {
-        root.right = removeNode(root.right, data);
-        return root;
-      } else if (data < root.data) {
-        root.left = removeNode(root.left, data);
-        return root;
-      } else {
-        if (!root.left && !root.right) return null;
-
-        if (!root.left) {
-          return root.right;
-        }
-        if (!root.right) {
-          return root.left;
-        }
-
-        let minRight = root.right;
-        while (minRight.left) {
-          minRight = minRight.left;
-        }
-        root.data = minRight.data;
-        root.right = removeNode(root.right, minRight.data);
-        return root;
-      }
-    }
-  }
-  min() {
-    let node = this.rootTree;
-    while (node.left) {
-      node = node.left;
-    }
-    return node.data;
-  }
-  max() {
-    let node = this.rootTree;
-    while (node.right) {
-      node = node.right;
-    }
-    return node.data;
-  }
-}
 
 module.exports = {
   Queue
