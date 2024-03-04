9505c81bab1ecd25ec4d2fb3f4781745d0154531
evoronova0202 <131252557+evoronova0202@users.noreply.github.com>
2024-03-04 23:13:07 +0300

    feat: add soluslitions on basic-js-ds task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,10 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function removeKFromList(l, k) {
+	if (l) {
+	  if (l.value === k) {
+		l = l.next;
+		return removeKFromList(l, k);
+	  }
+	  l.next = removeKFromList(l.next, k);
+	  return l;
+	}
+	return null;
+  }
 
 module.exports = {
   removeKFromList
